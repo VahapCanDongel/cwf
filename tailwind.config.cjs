278679375bcac853579490788c863d753719edf1
VahapCanDongel <vahapcandongel@hotmail.com>
2023-01-16 14:44:05 +0000

    flowbite added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
+  plugins: [require('flowbite/plugin')],
   theme: {
     extend: {},
     fontFamily: {
